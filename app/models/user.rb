af1be48fe10ea28dcb820c4e694244480af8651b
Tomo-pc <onga.tomo@icloud.com>
2023-10-25 00:18:56 +0900

    バリデーションの修正と正常系テストの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :buyers
-  has_many :orders
+  # has_many :items
+  # has_many :buyers
+  # has_many :orders
 
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々]+\z/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々]+\z/ }
-  validates :first_kana, presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ }
-  validates :last_kana, presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ }
+  validates :first_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day, presence: true
-  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/ }
+  validates :password, format: { with: /\A(?=.*[a-z])(?=.*\d).+\z/ }
 end
