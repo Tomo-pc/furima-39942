e33391b03ce956194b9cc017a6379b2385aa90fb
Tomo-pc <onga.tomo@icloud.com>
2023-11-02 19:19:51 +0900

    PullRequest前の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
   before_action :move_to_index, only: :index
 
   def index
+    unless user_signed_in?
+      redirect_to root_path
+    end
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
